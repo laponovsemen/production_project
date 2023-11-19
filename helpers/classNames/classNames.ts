e1e1b100962623f906ceb33f7bf2f4d025625e74
Laponov Semen <johndoe@example.com>
2023-11-20 03:48:40 +0400

    first commit

---
+++ helpers/classNames/classNames.ts
@@ -0,0 +1,18 @@
+
+type Mods = Record<string, boolean | string>
+const obj : Mods = {
+    "hovered" : true,
+
+}
+export function classNames(cls : string, mods : Mods, additional: string[]) : string {
+    return [
+      cls,
+        ...additional,
+        Object.entries(mods)
+            .filter(([className, value]) => Boolean(value))
+            .map(([className, value]) => className)
+    ].join(' ')
+}
+
+classNames("remove-btn", {hovered : true, selectable : true, red : true},['pdg'])
+
